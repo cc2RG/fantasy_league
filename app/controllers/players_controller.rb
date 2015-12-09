1fd12ddbae7adc9b943e4893bbcff20361de8be8
cc2RG <rbgalloway88@gmail.com>
2015-12-09 15:23:18 +0000

    forth commit

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,23 +1,40 @@
 class PlayersController < ApplicationController
 
-  def edit
-  end
-
   def index
+    @players = Player.all
   end
 
   def new
+    @player = Player.new
   end
+
+  def create
+    Player.create(player_params)
+    redirect_to(players_path)
+  end  
+
+
+  def edit
+    @player = Player.find(params[:id])
+  end
+
+  
 
   def show
+    @player = Player.find(params[:id])
   end
 
-
+  def destroy
+    player = Player.find(params[:id])
+    player.destroy
+    redirect_to( players_path )
+  end
 
 
   private
 
   def player_params
+    params.require(:player).permit(:name, :sex, :position, :offence, :defence, :popularity)
   end  
 
 end  
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,10 @@ Team.create!(name: 'Moutohora Macaws', founded: '1 March 1872', mascot: 'Sparky'
 Team.create!(name: 'Ballycastle Bats', founded: '6 November 1887', mascot: 'Barny the Fruitbat')
 Team.create!(name: 'Kenmare Kestrels', founded: '24 May 1909', mascot: 'Kez')
 
-Player.create!(name: "George Smith", sex: "M", position: "Chaser", offence: 7, defense: 3, popularity: 5)
-Player.create!(name: "Lesley Riddoch", sex: "F", position: "Chaser", offence: 8, defense: 4, popularity: 7)
-Player.create!(name: "Raymond Huntley", sex: "M", position: "Chaser", offence: 8, defense: 3, popularity: 8)
-Player.create!(name: "Billy James", sex: "M", position: "Beater", offence: 6, defense: 8, popularity: 6 )
+Player.create!(name: "George Smith", sex: "M", position: "Chaser", offence: 7, defence: 3, popularity: 5)
+Player.create!(name: "Lesley Riddoch", sex: "F", position: "Chaser", offence: 8, defence: 4, popularity: 7)
+Player.create!(name: "Raymond Huntley", sex: "M", position: "Chaser", offence: 8, defence: 3, popularity: 8)
+Player.create!(name: "Billy James", sex: "M", position: "Beater", offence: 6, defence: 8, popularity: 6 )
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
