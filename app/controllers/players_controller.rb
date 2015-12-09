465228af503c9942696ece973373ff43943dbc40
cc2RG <rbgalloway88@gmail.com>
2015-12-09 14:38:41 +0000

    added p controller and views

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,23 @@
+class PlayersController < ApplicationController
+
+  def edit
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+
+
+
+  private
+
+  def player_params
+  end  
+
+end  
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@ Team.create!(name: 'Ballycastle Bats', founded: '6 November 1887', mascot: 'Barn
 Team.create!(name: 'Kenmare Kestrels', founded: '24 May 1909', mascot: 'Kez')
 
 Player.create!(name: "George Smith", sex: "M", position: "Chaser", offence: 7, defense: 3, popularity: 5)
-
+Player.create!(name: "Lesley Riddoch", sex: "F", position: "Chaser", offence: 8, defense: 4, popularity: 7)
+Player.create!(name: "Raymond Huntley", sex: "M", position: "Chaser", offence: 8, defense: 3, popularity: 8)
+Player.create!(name: "Billy James", sex: "M", position: "Beater", offence: 6, defense: 8, popularity: 6 )
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
